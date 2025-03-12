c5f4ac38c6269474df834a0c422e72a3b9c51fe7
mattygersh21 <dakotaseia@hotmail.com>
2025-03-12 16:45:32 -0500

    - added findAllCourses().subscribe() tests and necessary HttpTestController.expectOne().flush() statment - added other expect() statement

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from "@angular/core/testing";
 import { CoursesService } from "./courses.service";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { COURSES } from "../../../../server/db-data";
 
 describe("CoursesService", () => {
 
@@ -16,8 +17,19 @@ describe("CoursesService", () => {
         httpTestingController = TestBed.inject(HttpTestingController);
     });
 
-    it('it should retrieve all courses', () => {
-
+    it('should retrieve all courses', () => {
+        coursesService.findAllCourses().
+            subscribe(courses => {
+                expect(courses).toBeTruthy('No courses returned');
+                expect(courses.length).toBe(12, "incorrect number of courses");
+                const course = courses.find(c => c.id == 12);
+                expect(course.titles.description).toBe("Angular Testing Course");
+            });
+        const req = httpTestingController.expectOne('/api/courses');
+        expect(req.request.method).toEqual('GET');
+        // the req.flush() method is needed for the expect() statements inside the subscribe method to be evaluted. see next note for more detail
+        // the above coursesService.findAllCourses() method call will trigger an http request to the /api/courses endpoint, however the subscribe method will not be called until the req.flush() method is called as it is what emits response data and completes the request. to put it another way, the req.flush method simulates the server response
+        req.flush({payload: Object.values(COURSES)});
     });
 
 });
